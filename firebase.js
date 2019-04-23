10428c5cfe1fc13b9b526def8187692b087d618c
ebonibell33 <ebonibell33@users.noreply.github.com>
2019-04-23 17:51:15 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyD9sjW_c5YiZAGDGDYw_RKZymOukt-O2_4',
-  authDomain: 'youtrition-286a7.firebaseapp.com',
-  databaseURL: 'https://youtrition-286a7.firebaseio.com',
-  projectId: 'youtrition-286a7',
-  storageBucket: 'youtrition-286a7.appspot.com',
-  messagingSenderId: '787682246160'
+  apiKey: "AIzaSyA__IwEAJrUcft93COu17I26oyVQwoHqlw",
+  authDomain: "youtrition-dummy.firebaseapp.com",
+  databaseURL: "https://youtrition-dummy.firebaseio.com",
+  projectId: "youtrition-dummy",
+  storageBucket: "youtrition-dummy.appspot.com",
+  messagingSenderId: "628818385926"
 };
 const app = firebase.initializeApp(config);
 
